7e74aa861f5ed04fe70fc2fd682d2248a138d304
Sean Mairs <secmairs@gmail.com>
2020-12-21 17:19:30 -0800

    lab2 submission: IntList

---
+++ LectureCode/IntList.java
@@ -0,0 +1,63 @@
+package LectureCode;
+
+public class IntList {
+	public int first;
+	public IntList rest;
+
+	public IntList(int f, IntList r) {
+		first = f;
+		rest = r;
+	}
+
+	/** Return the size of the list using... recursion! */
+	public int size() {
+		if (rest == null) {
+			return 1;
+		}
+		return 1 + this.rest.size();
+	}
+
+	/** Return the size of the list using no recursion! */
+	public int iterativeSize() {
+		IntList p = this;
+		int totalSize = 0;
+		while (p != null) {
+			totalSize += 1;
+			p = p.rest;
+		}
+		return totalSize;
+	}
+
+	/** Returns the ith item of this IntList. */
+	public int get(int i) {
+		if (i == 0) {
+			return first;
+		}
+		return rest.get(i - 1);
+	}
+   
+   public static IntList incrList(IntList l, int x){
+      if (l == null){
+         return null;
+      }
+      return new IntList(l.first+x, incrList(l.rest, x));
+   }
+   
+   public static IntList dincrList(IntList l, int x){
+      IntList p = l;
+      IntList dinc = l;
+      while (p!= null){
+         p.first = p.first + x;
+         p = p.rest;
+      }
+      return dinc;
+   }
+
+	public static void main(String[] args) {
+		IntList L = new IntList(15, null);
+		L = new IntList(10, L);
+		L = new IntList(5, L);
+
+		IntList x = dincrList(L, 5);
+	}
+} 
