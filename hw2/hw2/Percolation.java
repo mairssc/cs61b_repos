65674fe63a48c6061c91b5a22fd6447c84ce4b39
Sean Mairs <secmairs@gmail.com>
2021-01-06 12:08:39 -0800

    HW 2 Submission

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -4,4 +4,131 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
+    private WeightedQuickUnionUF PerUnion;
+    private WeightedQuickUnionUF PerUnionFull;
+    private int r;
+    private boolean[][] twoDArray;
+    private int openSites;
+    private int virTop;
+    private int virBot;
+
+    private boolean checkBounds(int row, int col) {
+        if (row >= r || col >= r || row < 0 || col < 0) {
+            return false;
+        }
+        return true;
+    }
+
+    private int twoDToOneD(int row, int col) {
+        return (row * r) + col;
+    }
+
+    private boolean possibleFill(int row, int col) {
+        if (checkBounds(row, col)) {
+            return twoDArray[row][col];
+        }
+        return false;
+    }
+
+    // create N-by-N grid, with all sites initially blocked
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("Illegal Argument");
+        }
+        PerUnion = new WeightedQuickUnionUF(N * N + 2);
+        PerUnionFull = new WeightedQuickUnionUF(N * N + 1);
+        r = N;
+        twoDArray = new boolean[N][N];
+        openSites = 0;
+        virTop = N * N;
+        virBot = N * N + 1;
+    }
+
+    // open the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        if (!checkBounds(row, col)) {
+            throw new java.lang.IndexOutOfBoundsException("Invalid arguments");
+        }
+        twoDArray[row][col] = true;
+        openSites += 1;
+        int index = twoDToOneD(row, col);
+
+        boolean up = possibleFill(row + 1, col);
+        boolean down = possibleFill(row - 1, col);
+        boolean right = possibleFill(row, col + 1);
+        boolean left = possibleFill(row, col - 1);
+
+        if (row == 0) {
+            PerUnion.union(index, virTop);
+            PerUnionFull.union(index, virTop);
+        } else if (row == r - 1) {
+            PerUnion.union(index, virBot);
+        }
+
+        if (up) {
+            PerUnion.union(index, twoDToOneD(row + 1, col));
+            PerUnionFull.union(index, twoDToOneD(row + 1, col));
+        }
+        if (down) {
+            PerUnion.union(index, twoDToOneD(row - 1, col));
+            PerUnionFull.union(index, twoDToOneD(row - 1, col));
+        }
+        if (right) {
+            PerUnion.union(index, twoDToOneD(row, col + 1));
+            PerUnionFull.union(index, twoDToOneD(row , col + 1));
+        }
+        if (left) {
+            PerUnion.union(index, twoDToOneD(row, col - 1));
+            PerUnionFull.union(index, twoDToOneD(row, col - 1));
+        }
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        if (!checkBounds(row, col)) {
+            throw new java.lang.IndexOutOfBoundsException("Invalid arguments");
+        }
+        return twoDArray[row][col];
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        if (!checkBounds(row, col)) {
+            throw new java.lang.IndexOutOfBoundsException("Invalid arguments");
+        }
+        int index = twoDToOneD(row, col);
+        return PerUnionFull.connected(index, virTop);
+    }
+
+    // number of open sites
+    public int numberOfOpenSites() {
+        return openSites;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return PerUnion.connected(virTop, virBot);
+    }
+
+    // use for unit testing (not required, but keep this here for the autograder
+    public static void main(String[] args) {
+        Percolation p = new Percolation(10);
+        p.open(3, 4);
+        p.open(2, 4);
+        p.open(2, 2);
+        p.open(2, 3);
+        p.open(0, 2);
+        p.open(4, 4);
+        p.open(1, 2);
+        p.open(4, 0);
+        System.out.println(p.isOpen(3, 4));
+        System.out.println(p.isOpen(2, 4));
+        System.out.println(p.isOpen(2, 2));
+        System.out.println(p.openSites);
+        System.out.println(p.isFull(0, 2));
+        System.out.println(p.isFull(4, 4));
+        System.out.println(p.percolates());
+        System.out.println(p.isFull(4, 0));
+        System.out.println(p.PerUnion.count());
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,86 @@
 package hw2;
 
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+import edu.princeton.cs.introcs.Stopwatch;
+
+
 public class PercolationStats {
 
+    private PercolationFactory factory;
+    private double[] percThreshold;
+
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        factory = pf;
+        Percolation curPerc;
+        percThreshold = new double[T];
+        double totalSites = N * N;
+        int index = 0;
+        int randRow = 0;
+        int randCol = 0;
+        while (T > 0) {
+            curPerc = factory.make(N);
+            while (!curPerc.percolates()) {
+                randRow = StdRandom.uniform(0, N);
+                randCol = StdRandom.uniform(0, N);
+                if (!curPerc.isOpen(randRow, randCol)) {
+                    curPerc.open(randRow, randCol);
+                }
+            }
+            percThreshold[index] = (double) curPerc.numberOfOpenSites()/totalSites;
+            index += 1;
+            T -= 1;
+        }
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(percThreshold);
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(percThreshold);
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLow() {
+        return mean() - ((1.96 * stddev()) / (Math.sqrt((double) percThreshold.length)));
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHigh() {
+        return mean() + ((1.96 * stddev()) / (Math.sqrt((double) percThreshold.length)));
+    }
+
+    public static void main(String[] args) {
+        PercolationFactory x = new PercolationFactory();
+        Stopwatch watch;
+        int index = 0;
+        int index2 = 0;
+        Percolation cur;
+        PercolationStats stats;
+        double[] timeArray = new double[20];
+        double[] timeArray2 = new double[20];
+        for (int i = 10; i < 1000; i *= 2) {
+            watch = new Stopwatch();
+            cur = x.make(i);
+            timeArray[index] = watch.elapsedTime();
+            index += 1;
+        }
+        for (int i = 0; i < timeArray.length; i++) {
+            System.out.println(timeArray[i]);
+        }
+        System.out.println();
+        for (int t = 10; t < 1000; t *= 2) {
+            watch = new Stopwatch();
+            stats = new PercolationStats(100, t, x);
+            timeArray2[index] = watch.elapsedTime();
+            index2 += 1;
+        }
+        for (int i = 0; i < timeArray2.length; i++) {
+            System.out.println(timeArray2[i]);
+        }
+    }
 }
